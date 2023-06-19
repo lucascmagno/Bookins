3fe7e809c1304cd6e7da02ce1b2c531b81001c3c
Lucas Chagas Magno <109705062+lucascmagno@users.noreply.github.com>
2023-06-19 18:07:09 -0300

    Listar livros Dao criado

--- src/Bookins/dao/SelectDao.java
+++ src/Bookins/dao/SelectDao.java
@@ -1,22 +1,47 @@
 package Bookins.dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
+import Bookins.model.Livro;
 //Conexao
 import Bookins.util.Conexao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelectDao {
-	public ResultSet listarLivros() {
-		
-		Conexao con = null;
-		
-		try {
-			con = new Conexao();
-			con.executeQuery("SELECT * FROM livro");
-		}catch(SQLException e){
-			System.out.print(e);
-		}
-		return null;
-	}
+	 private Connection conexao; // A conexão com o banco de dados
+
+	    // Construtor que recebe a conexão com o banco de dados
+	    public SelectDao(Connection conexao) {
+	        this.conexao = conexao;
+	    }
+	    
+    public List<Livro> listarLivros() throws SQLException {
+        List<Livro> livros = new ArrayList<>();
+        
+        //Conexao con = null;
+        // Cria a consulta SQL
+        String sql = "SELECT * FROM livro";
+
+        try (Statement stmt = conexao.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+        	//con = new Conexao();
+            // Processa o resultado
+            while (rs.next()) {
+                // Crie uma instância de Livro e preencha os dados
+                Livro livro = new Livro();
+                livro.setId(rs.getInt("id"));
+                livro.setTitulo(rs.getString("titulo"));
+                livro.setDescricao(rs.getString("descricao"));
+                livro.setPreco(rs.getDouble("preco"));
+                // Adicione o livro à lista
+                livros.add(livro);
+            }
+        }
+
+        return livros;
+    }
 }
